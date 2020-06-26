0681e2815b081f893ef62b2e3b2873d700e35c14
Kiri <hello.kiri.lee@gmail.com>
2020-06-26 21:08:02 +0800

    Mobile Menu Created

--- navbar.js
+++ navbar.js
@@ -1,5 +1,9 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+<<<<<<< Updated upstream
 function myFunction() {
+=======
+function mobilenav() {
+>>>>>>> Stashed changes
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
