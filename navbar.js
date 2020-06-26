15363e8d3d569083a01eaa6a2975ddb109d48b23
Kiri <hello.kiri.lee@gmail.com>
2020-06-26 22:35:24 +0800

    Nav menu created & working

--- navbar.js
+++ navbar.js
@@ -1,13 +1,10 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-<<<<<<< Updated upstream
-function myFunction() {
-=======
+/* Mobile - Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger */
 function mobilenav() {
->>>>>>> Stashed changes
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
-    x.style.display = "none";
+      x.style.display = "none";
   } else {
-    x.style.display = "block";
+      x.style.display = "block";
   }
-}
+  }
+
